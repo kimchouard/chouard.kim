f7156e498d1c566d289846b59ee3eef5dd615828
Kim Chouard <k.chouard@salesforce.com>
2020-03-09 00:51:27 -0700

    Website revamp and moved to chouard.kim

--- js/anim.js
+++ js/anim.js
@@ -1,5 +1,5 @@
 //Animation d'ouverture OU d'adaptation de la bulle 
-function ouvrirBulle(ratioB, timing, speed, fonctionFin) {
+const ouvrirBulle = (ratioB, timing, speed, fonctionFin) => {
 	//Variables utiles
 	var haut = $(window).height();
 	var larg = $(window).width();
@@ -31,32 +31,16 @@ function ouvrirBulle(ratioB, timing, speed, fonctionFin) {
 	});
 };
 
-function animerIcones(top, bottom) {
+const animerIcones = (top, bottom) => {
 	if (hover == false) {
-        $("#contact a").each(function(index) {
+        $(".menu a").each(function(index) {
             console.info(index);
-            $($("#contact a")[index]).delay(index*50).animate({top: top+'px'}, 300).animate({top: bottom+'px'}, 300);
+            $($(".menu a")[index]).delay(index*50).animate({top: top+'px'}, 300).animate({top: bottom+'px'}, 300);
         });
 	};
 };
 
-function  lunchHome() {
-	$('#wrapPorto').fadeOut(300, function() {
-		$('#main').fadeIn(0);
-	});
-};
-
-function animBulle(id) {
-	$('#'+id).hover(function() {
-		$('#'+id+' h3').stop().animate({top: '-100px'}, 100);
-		$('#'+id+' svg').stop().fadeIn(0).animate({top: '-100px'}, 100);
-	}, function() {
-		$('#'+id+' h3').stop().fadeIn().animate({top: '0px'}, 200);
-		$('#'+id+' svg').stop().fadeOut(0).animate({top: '20px'}, 100);
-	});
-};
-
-function scrollThumbnails(div) {
+const scrollThumbnails = (div) => {
 	//Adapte la largeur de thumbnail pour que toutes créactions apparaissent
 	nbLiens = $('#wrap'+div+' .thumbnail a').get().length;		
 	$('#wrap'+div+' .thumbnail').css({width: nbLiens*230}); //premier lien + autres liens
@@ -75,95 +59,12 @@ function scrollThumbnails(div) {
 	});
 };
 
-function  lunchCatPorto(div) {
-	$('#bulles').fadeOut(200, function() {
-		$('#wrap'+div).fadeIn(200);
-		$('#websites svg').stop().fadeOut(0).animate({top: '20px'}, 100);
-		$('#movies svg').stop().fadeOut(0).animate({top: '20px'}, 100);
-		$('#visuals svg').stop().fadeOut(0).animate({top: '20px'}, 100);
-		
-		$('#wrapWebsites .thumbnail a').hover(function() {
-			$($(this).find('.color')).stop().fadeIn(200);
-		}, function() {
-			$($(this).find('.color')).stop().fadeOut(200);
-		});
-		
-		scrollThumbnails('Websites');
-		scrollThumbnails('Visuals');
-		scrollThumbnails('Movies');
-	});
-};
-
-function  returnPorto() {
-	$('#wrapWebsites').fadeOut(200);
-	$('#wrapMovies').fadeOut(200);
-	$('#wrapVisuals').fadeOut(200)
-	$('#bulles').delay(200).fadeIn(200);
-};
-
-function  lunchPorto() {
-	$('#main').fadeOut(300, function() {
-		$('#wrapPorto').fadeIn(200);
-		timingCercle = 800;
-		$('#websites').animate({
-			path : new $.path.bezier({
-			 start: { 
-				x: 272, 
-				y: 98, 
-				angle: 0
-			},  
-			end: { 
-				x:272,
-				y:-50, 
-				angle: 100, 
-				length: 0.25
-			}
-		})},timingCercle);
-		$('#visuals').animate({
-			path : new $.path.bezier({
-			 start: { 
-				x: 112, 
-				y: 98, 
-				angle: 0
-			},  
-			end: { 
-				x:-120,
-				y:200, 
-				angle: 100, 
-				length: 0.25
-			}
-		})},timingCercle);
-		$('#movies').animate({
-			path : new $.path.bezier({
-			 start: { 
-				x: -52, 
-				y: 98, 
-				angle: 0
-			},  
-			end: { 
-				x:120,
-				y:200, 
-				angle: 100, 
-				length: 0.25
-			}
-		})},timingCercle, function() {
-			$('.wrapBulle h3').fadeIn(100, function() {
-				animBulle('websites');
-				animBulle('movies');
-				animBulle('visuals');
-			});
-		});
-	});
-};
-
 //constantes initiales
-var ratio = 1.05;
-var speed = 0.8;
-var hover = false;
+const ratio = 1.05;
+const speed = 0.8;
+let hover = false;
 
-$(function() {
-	
-	
+$(() => {
 	//Préparation de la scène
 	$('#main').animate({right: 0, bottom: 0}, 600);
 	$('#wrapTexte').css({left: $(window).width()/2, top: $(window).height()/2, height: 0, width: 0});
@@ -173,25 +74,26 @@ $(function() {
 	ouvrirBulle(ratio, 600, speed, true);
 	
 	//Adaptation lorsqu'on redimenssione la fenètre
-	$(window).resize(function() { ouvrirBulle(ratio, 600, speed, false); });
+	$(window).resize(() => { ouvrirBulle(ratio, 600, speed, false); });
 	
 	//Au survol des icones.
 	$('.item').each(function() {
-		$(this).hover(function() {
+		$(this).hover(() => {
 			hover = true;
 			text = $(this).attr('alt');
-			$('#details').html(text).fadeIn();
+			$(`#${ $(this).parent().parent().attr('id') } .details`).html(text).fadeIn();
 			$(this).animate({top: '30px'}, 200);
-		}, function() {
+		}, () => {
 			hover = false;
 			$(this).animate({top: '50px'}, 100);
+			$(`#${ $(this).parent().parent().attr('id') } .details`).html(($(this).parent().parent().attr('id') === 'wrapSDG') ? 'SDG Advocacy & Education' : 'Creativity');
 		});
 		
-		$('#wrapContact').hover(function() { }, function() {
-			$('#details').html('').fadeOut();
-		});
+		// $('.wrapSDG').hover(() => { }, () => {
+		// 	$('.wrapSDG .details').html('').fadeOut();
+		// });
 	});
 	
 	//Gestion animation icones.
-	setInterval(function() { animerIcones(30,50); }, 5000);
+	setInterval(() => { animerIcones(30,50); }, 5000);
 });
