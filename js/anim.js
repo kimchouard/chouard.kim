416c70d0d5da1a305452dc2e8e47f0200f7f56da
Kim Chouard <k.chouard@salesforce.com>
2020-05-19 19:32:56 -0700

    Mobile responsive. Need to fix intro animation

--- js/anim.js
+++ js/anim.js
@@ -16,19 +16,27 @@ const ouvrirBulle = (ratioB, timing, speed, fonctionFin) => {
 	var padding = (taille*1.5-$('#main').width())/2;
 	
 	//Animation d'entrée
-	$('#wrapTexte').stop().animate({width: $('#main').width(), height: taille, 'padding-top': (taille)/2, 'padding-left': padding, 'padding-right': padding, top: ($(window).height()-(taille*1.5))/2, left: ($(window).width()-(taille*1.5))/2}, timing, function() {
-		//Affichage de la citation
-		if (fonctionFin) {
-			// $('#pli').stop().animate({left:-20, top: 59}, 200*speed, function() {
-			// 	$('#pli').stop().animate({left: 295, top: -100}, 1100*speed);
-			// 	$('#wrapCitation').stop().animate({left: 0, top: 0}, 1000*speed);
-			// 	$('#citation').stop().animate({left: 0}, 1000*speed, function() {
-			// 		//Animation de fin de chargement
-			// 		animerIcones(30, 50);
-			// 	});
-			// });
-		};
-	});
+	// $('#wrapTexte').stop().animate({
+	// 	width: '100%', //$('#main').width(), 
+	// 	height: taille,
+	// 	// 'padding-top': (taille)/2,
+	// 	// 'padding-left': padding,
+	// 	// 'padding-right': padding,
+	// 	// top: ($(window).height()-(taille*1.5))/2, 
+	// 	// left: ($(window).width()-(taille*1.5))/2},
+	// }, timing, function() {
+	// 	//Affichage de la citation
+	// 	if (fonctionFin) {
+	// 		// $('#pli').stop().animate({left:-20, top: 59}, 200*speed, function() {
+	// 		// 	$('#pli').stop().animate({left: 295, top: -100}, 1100*speed);
+	// 		// 	$('#wrapCitation').stop().animate({left: 0, top: 0}, 1000*speed);
+	// 		// 	$('#citation').stop().animate({left: 0}, 1000*speed, function() {
+	// 		// 		//Animation de fin de chargement
+	// 		// 		animerIcones(30, 50);
+	// 		// 	});
+	// 		// });
+	// 	};
+	// });
 };
 
 const animerIcones = (top, bottom) => {
@@ -65,27 +73,27 @@ const speed = 0.8;
 let hover = false;
 
 $(() => {
-	//Préparation de la scène
-	$('#main').animate({right: 0, bottom: 0}, 600);
-	$('#wrapTexte').css({left: $(window).width()/2, top: $(window).height()/2, height: 0, width: 0});
-	$('body').css({height: $(window).height()});
+	// //Préparation de la scène
+	// $('#main').animate({right: 0, bottom: 0}, 600);
+	// // $('#wrapTexte').css({left: $(window).width()/2, top: $(window).height()/2, height: 0, width: 0});
+	// $('body').css({height: $(window).height()});
 	
-	//Lancement de l'animation
-	ouvrirBulle(ratio, 600, speed, true);
+	// //Lancement de l'animation
+	// ouvrirBulle(ratio, 600, speed, true);
 	
-	//Adaptation lorsqu'on redimenssione la fenètre
-	$(window).resize(() => { ouvrirBulle(ratio, 600, speed, false); });
+	// //Adaptation lorsqu'on redimenssione la fenètre
+	// $(window).resize(() => { ouvrirBulle(ratio, 600, speed, false); });
 	
-	//Au survol des icones.
+	// //Au survol des icones.
 	$('.item').each(function() {
 		$(this).hover(() => {
 			hover = true;
 			text = $(this).attr('alt');
 			$(`#${ $(this).parent().parent().attr('id') } .details`).html(text).fadeIn();
-			$(this).animate({top: '30px'}, 200);
+			// $(this).animate({top: '0px'}, 200);
 		}, () => {
 			hover = false;
-			$(this).animate({top: '50px'}, 100);
+			// $(this).animate({top: '15px'}, 100);
 			$(`#${ $(this).parent().parent().attr('id') } .details`).html(($(this).parent().parent().attr('id') === 'wrapSDG') ? 'SDG Advocacy & Education' : 'Creativity');
 		});
 		
@@ -94,6 +102,6 @@ $(() => {
 		// });
 	});
 	
-	//Gestion animation icones.
-	setInterval(() => { animerIcones(30,50); }, 5000);
+	// //Gestion animation icones.
+	// setInterval(() => { animerIcones(0,15); }, 5000);
 });
